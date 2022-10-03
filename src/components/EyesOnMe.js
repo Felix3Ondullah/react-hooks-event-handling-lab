f8d00a8c15c56f8ca89d2bff32fcfc9fde047519
Felix Ondullah <felixondullah@gmail.com>
2022-10-03 18:51:12 +0300

    test passed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,20 +1,13 @@
-// Code EyesOnMe Component Here
 import React from "react";
+// Code EyesOnMe Component Here
 
-const EyesOnMe = ()=>
-{
-    const handleFocus =()=>
-    {
-        console.log("Good!")
-    }
-    const handleBlur = () =>
-    {
-        console.log("Hey! Eyes on me!")
-    }
-    return (
-      <>
-        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
-      </>
-    );
+
+const EyesOnMe = () => {
+    const setFocus = () => console.log("Good!");
+    const setblur = () => console.log("Hey! Eyes on me!");
+
+    return <button onFocus={setFocus} onBlur={setblur}>Eyes on me</button>
 }
+
+
 export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,23 +1,12 @@
-// Code Keypad Component Here
 import React from "react";
+// Code Keypad Component Here
 
-const Keypad =()=>
-{
-    const keypadFn = ( eventFunction ) =>
-    {
-        console.log(eventFunction.target.name);
-        // console.log("Entering password...");
-    }
-    return (
-      <>
-        <input
-          type="password"
-          name="Entering password..."
-          onChange={keypadFn}
-          placeholder="Enter your password..."
-        />
-      </>
-    );
-    
+
+const Keypad = () => {
+    const change = () => console.log("Entering password...")
+
+    return <input onChange={change} type="password" />
+
 }
+
 export default Keypad;
